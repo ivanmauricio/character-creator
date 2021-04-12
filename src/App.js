c15f2b285fe7adc157458d77f8025f1cf760800d
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2021-04-12 12:01:57 +0100

    complete character map exercise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          bottom: "0",
+          height: "40vh",
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+          width: "100%",
+        }}
+      />
       <CharacterEditor />
       <Footer />
     </>
